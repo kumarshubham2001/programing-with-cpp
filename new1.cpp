fe61df24e2fa2df243b871b4360bed455abef070
Shubham Kumar <shubham07pal@gmail.com>
2022-08-24 23:01:28 +0530

    first step git learning

---
+++ new1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+void solve()
+{
+    
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t ; 
+    cin>>t ; 
+    while( t-- > 0 )
+    {
+        solve() ;
+    }
+}
